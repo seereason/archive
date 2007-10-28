5cd275dce23a54bb71e0fa47e530fcf96c0bd43e
David Fox <dsf@seereason.org>
2007-10-28 19:10:11 +0000

    Fix too many open file handles bug

--- FindCopies.hs
+++ FindCopies.hs
@@ -4,13 +4,16 @@
 -- comparing the files in each size group.
 module Main where
 
+import Control.Exception
 import System.Directory
 import System.IO
 import System.Environment
 import System.Posix.Files
 import System.Posix.Types
 import Data.List
+import Data.Maybe
 import qualified Data.ByteString.Lazy as B
+--import qualified Data.ByteString as B
 
 -- Lets try not to get these mixed up...
 type CopyPath = FilePath
@@ -22,33 +25,43 @@ main =
          [original, copy] ->
              do originals <- findFiles original >>= return . groupFiles
                 candidates <- findFiles copy >>= return . groupFiles
-                copies <- findCopies originals candidates
-                mapM_ (hPutStrLn stderr . show) copies
+                findCopies (hPutStrLn stderr . show) originals candidates
          _ -> error $ "Usage: findcopies originaldir copydir"
 
 -- |Return a list of all pairs where one file is in the copy directory and one is in the original.
-findCopies :: [(FileOffset, [OrigPath])] -> [(FileOffset, [CopyPath])] -> IO [(OrigPath, CopyPath)]
-findCopies _ [] = return []
-findCopies [] _ = return []
-findCopies originals@((originalSize, originalPaths) : moreOriginals) copies@((copySize, copyPaths) : moreCopies) =
+findCopies :: ((OrigPath, CopyPath) -> IO ()) -> [(FileOffset, [OrigPath])] -> [(FileOffset, [CopyPath])] -> IO ()
+findCopies _ _ [] = return ()
+findCopies _ [] _ = return ()
+findCopies f originals@((originalSize, originalPaths) : moreOriginals) copies@((copySize, copyPaths) : moreCopies) =
     case compare originalSize copySize of
-      GT -> findCopies moreOriginals copies
-      LT -> findCopies originals moreCopies
-      EQ -> do originalPairs <- mapM B.readFile originalPaths >>= return . zip originalPaths
-               copyPairs <- mapM B.readFile copyPaths >>= return . zip copyPaths
-               -- hPutStrLn stderr $ ("Size: " ++ show originalSize ++
-               --                     ", originals: " ++ show (length originalPaths) ++
-               --                     ", copies: " ++ show (length (copyPaths)))
-               let matches = concat . map (findOriginal originalPairs) $ copyPairs
-               -- case matches of [] -> return (); _ -> hPutStrLn stderr $ ("  Match: " ++ show matches)
-               more <- findCopies moreOriginals moreCopies
-               return (matches ++ more)
+      GT -> findCopies f moreOriginals copies
+      LT -> findCopies f originals moreCopies
+      EQ -> compareFiles originalPaths copyPaths >>
+            findCopies f moreOriginals moreCopies
     where
+      compareFiles originalPaths copyPaths =
+          do originalHandles <- mapM open originalPaths
+             originalText <- mapM read originalHandles
+             copyHandles <- mapM open copyPaths
+             copyText <- mapM read copyHandles
+             let originalTriples = catMaybes . map zapMissing $ zip3 originalPaths originalHandles originalText
+             let copyTriples = catMaybes . map zapMissing $ zip3 copyPaths copyHandles copyText
+             let matches = concat . map (findOriginal originalTriples) $ copyTriples
+             mapM_ f matches
+             mapM_ (\ (_, handle, _) -> hClose handle) (originalTriples ++ copyTriples)
       -- Compare the text of the copy to the text of each original, and return the
       -- path pair of the first match.
-      findOriginal :: [(OrigPath, B.ByteString)] -> (CopyPath, B.ByteString) -> [(OrigPath, CopyPath)]
-      findOriginal origPairs (copyPath, copyText) =
-          maybe [] (\ pr -> [((fst pr), copyPath)]) (find ((==) copyText . snd) origPairs)
+      findOriginal :: [(OrigPath, Handle, B.ByteString)] -> (CopyPath, Handle, B.ByteString) -> [(OrigPath, CopyPath)]
+      findOriginal origTriples (copyPath, _, copyText) =
+          maybe [] (\ (path, _, _) -> [(path, copyPath)]) (find (\ (_, _, text) -> text == copyText) origTriples)
+      open :: FilePath -> IO (Maybe Handle)
+      open path = try (openBinaryFile path ReadMode) >>= either (return . const Nothing) (return . Just)
+      read :: Maybe Handle -> IO (Maybe B.ByteString)
+      read = maybe (return Nothing) (\ h -> B.hGetContents h >>= return . Just)
+      omitFailed :: [(FilePath, Maybe Handle, Maybe B.ByteString)] -> [(FilePath, Handle, B.ByteString)]
+      omitFailed = catMaybes . map zapMissing
+      zapMissing (a, Just b, Just c) = Just (a, b, c)
+      zapMissing _ = Nothing
 
 -- |Find all the non-directory files within a directory.  Returns a
 -- list of each file's size and path.
